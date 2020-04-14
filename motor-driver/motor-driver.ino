16a5ce0aa2c780276bff9a065acc15a005149528
Eugenio Miró <eugenio.miro@gmail.com>
2020-04-14 10:35:16 -0300

    velocidad variable

--- motor-driver/motor-driver.ino
+++ motor-driver/motor-driver.ino
@@ -1,15 +1,40 @@
-int wait = 25;
+
+int wait   = 25;
 int yellow = 2;
-int green = 3;
-int red = 4;
+int green  = 3;
+int red    = 4;
+bool started = true;
 
 void setup() { 
+    Serial.begin(9600);
     pinMode(yellow, OUTPUT);
-    pinMode(green, OUTPUT);
-    pinMode(red, OUTPUT);
+    pinMode(green,  OUTPUT);
+    pinMode(red,    OUTPUT);
 }
 
 void loop() {
+    if (Serial.available()) {
+        int c = Serial.read();
+
+        Serial.println(c);
+        switch(c) {
+            case 'j':
+                wait += 1;
+                Serial.println(wait);
+                break;
+            case 'k':
+                wait -= 1;
+                Serial.println(wait);
+                break;
+            case 's':
+                started = !started;
+                break;
+        }
+    }
+    if (!started) {
+        delay(1);
+        return;
+    }
     digitalWrite(yellow, 1); 
     digitalWrite(green, 1);
     digitalWrite(red, 0);
