d4310e51df52108343c2a3ea37063c64bdea92e5
Eugenio Miró <eugenio.miro@gmail.com>
2020-04-20 00:16:43 -0300

    add fan-rpm project

---
+++ fan-rpm/fan-rpm.ino
@@ -0,0 +1,67 @@
+//project done by www.theorycircuit.com
+//code by Crenn from http://thebestcasescenario.com  thank you!
+//Varibles used for calculations
+int NbTopsFan;
+int calc1;
+int calc2;
+
+//The pin location of the sensor
+int hallsensor = 2; 
+
+//Defines the structure for multiple fans and 
+//their dividers 
+typedef struct{
+    char fantype;
+    unsigned int fandiv; 
+} fanspec;
+ 
+//Definitions of the fans
+//This is the varible used to select the fan and it's divider,
+//set 1 for unipole hall effect sensor
+//and 2 for bipole hall effect sensor
+fanspec fanspace[3] = {{0,1},{1,2},{2,8}}; 
+char fan = 1;
+ 
+//This is the function that the interupt calls
+void rpm ()
+{
+    NbTopsFan++;
+}
+ 
+//This is the setup function where the serial port is initialised,
+//and the interrupt is attached
+void setup()
+{
+    pinMode(hallsensor, INPUT_PULLUP);
+    Serial.begin(9600);
+    attachInterrupt(0, rpm, RISING);
+}
+ 
+void loop ()
+{
+    //Set NbTops to 0 ready for calculations
+    NbTopsFan = 0;
+    //Enables interrupts
+    sei();
+ 
+    //Wait 1 second
+    delay (1000);
+ 
+    //Disable interrupts
+    cli();
+ 
+    //Times NbTopsFan (which is apprioxiamately the fequency the fan 
+    //is spinning at) by 60 seconds before dividing by the fan's divider
+    calc1 = ((NbTopsFan * 60) / fanspace[fan].fandiv);
+    calc2 = ((NbTopsFan * 60) / fanspace[2].fandiv); 
+
+    if (Serial.available()) { 
+        //Prints the number calculated above
+        Serial.print("calc1 = ");
+        Serial.print(calc1, DEC);
+        Serial.println (" rpm");
+        Serial.print("calc2 = ");
+        Serial.print (calc2, DEC);
+        Serial.println (" rpm");
+    }
+} 
