7375c4212c44b5e4096f7208ebe70fc1cf2a5329
Eugenio Miró <eugenio.miro@gmail.com>
2020-04-26 18:10:20 -0300

    use direct positioning escape secuences in an ansi terminal

--- fan-rpm/fan-rpm.ino
+++ fan-rpm/fan-rpm.ino
@@ -1,8 +1,11 @@
 //project done by www.theorycircuit.com
 //code by Crenn from http://thebestcasescenario.com  thank you!
 //Varibles used for calculations
+#include <stdio.h>
+
 int NbTopsFan;
-int calc;
+int fanSpeed;
+int maxSpeed;
 
 //The pin location of the sensor
 int hallsensor = 2; 
@@ -51,12 +54,39 @@ void loop ()
  
     //Times NbTopsFan (which is apprioxiamately the fequency the fan 
     //is spinning at) by 60 seconds before dividing by the fan's divider
-    calc = ((NbTopsFan * 60) / fanspace[fan].fandiv);
+    fanSpeed = ((NbTopsFan * 60) / fanspace[fan].fandiv);
 
     if (Serial.available()) { 
-        //Prints the number calculated above
-        Serial.print("calc = ");
-        Serial.print (calc, DEC);
-        Serial.println (" rpm");
+        printFanSpeed(fanSpeed);
+        if (fanSpeed > maxSpeed) {
+            maxSpeed = fanSpeed;
+            printMaxSpeed(maxSpeed);
+        }
     }
 } 
+
+void printFanSpeed(int rpm)
+{
+    moveXY(1, 1);
+    printSpeed("fan speed", rpm);
+}
+
+void printMaxSpeed(int rpm)
+{
+    moveXY(2, 1);
+    printSpeed("max speed", rpm);
+}
+
+void printSpeed(const char* label, int rpm)
+{
+    char buffer[100];
+    sprintf(buffer, "%-20s = %4d rpm", label, rpm);
+    Serial.print(buffer);
+}
+
+void moveXY(int x, int y)
+{
+    char cm[15];
+    sprintf(cm, "\033[%d;%dH", x + 1, y + 1);
+    Serial.print(cm);
+}
