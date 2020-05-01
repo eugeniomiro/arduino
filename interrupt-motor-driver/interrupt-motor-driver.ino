5de520c9e5b0d3bee799b5dc26385d67680dae32
Eugenio Miró <eugenio.miro@gmail.com>
2020-05-01 20:53:40 -0300

    replace loop code with interrupt code

--- interrupt-motor-driver/interrupt-motor-driver.ino
+++ interrupt-motor-driver/interrupt-motor-driver.ino
@@ -4,22 +4,20 @@ const uint8_t ledPin = 5;
 
 // Globals
 uint8_t led_state = LOW;
-uint8_t btn_prv = HIGH;
 
 void setup() {
   pinMode(btnPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(btnPin), toggle, FALLING);
 }
 
 void loop() {
-  // Poll for button push
-  uint8_t btn_state = digitalRead(btnPin);
-  if ((btn_state == LOW) && (btn_prv == HIGH)) {
-    led_state = !led_state;
-    digitalWrite(ledPin, led_state);
-  }
-  btn_prv = btn_state;
-
   // Pretend we're doing other stuff
   delay(500);
+}
+
+void toggle() {
+  led_state = !led_state;
+  digitalWrite(ledPin, led_state);
 }
