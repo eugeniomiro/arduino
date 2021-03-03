2b3b01cc78676ea2da08f0f2dc05d710396aacf0
Eugenio Miró <eugenio.miro@gmail.com>
2021-03-02 22:50:27 -0300

    add Capitulo5 code

---
+++ Curso/Capitulo5/caso1/caso1.ino
@@ -0,0 +1,29 @@
+int TRIG = 10;
+int ECO = 9;
+int LED = 3;
+int DURACION;
+int DISTANCIA;
+
+void setup() {
+	pinMode(TRIG, OUTPUT);
+	pinMode(ECO, INPUT);
+	pinMode(LED, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop() {
+	digitalWrite(TRIG, HIGH);
+	delay(1);
+	digitalWrite(TRIG, LOW);
+	DURACION = pulseIn(ECO, HIGH);
+	DISTANCIA = DURACION / 58.2;	// esta constante depende del sensor y transforma el valor a CM
+	Serial.println(DISTANCIA);
+	delay(200);
+	if (DISTANCIA <= 20 && DISTANCIA > 0) {
+		digitalWrite(LED, HIGH);
+		delay(DISTANCIA * 10);
+		digitalWrite(LED, LOW);
+	} else if (DISTANCIA <= 0) {
+		Serial.println("error!");
+	}
+}
