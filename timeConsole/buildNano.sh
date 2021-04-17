f7bc9f4ae639f54650b4d85d5062b2b652ed72e1
Eugenio Miró <eugenio.miro@gmail.com>
2021-04-17 20:41:31 -0300

    add conditional compilation in case compile fails

--- timeConsole/buildNano.sh
+++ timeConsole/buildNano.sh
@@ -1,2 +1,5 @@
 arduino-cli compile -b arduino:avr:nano:cpu=atmega328old timeConsole
-arduino-cli upload -b arduino:avr:nano:cpu=atmega328old timeConsole -p /dev/ttyUSB0
+exit_code=$?
+if [ $exit_code -eq 0 ]; then
+    arduino-cli upload -b arduino:avr:nano:cpu=atmega328old timeConsole -p /dev/ttyUSB0
+fi
